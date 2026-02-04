8bf5db3bbc4589224d83de48abc3a3918cc16c8d
FrancescoSpinella3 <francescospinella76gmail.com>
2026-02-04 18:38:24 +0100

    styling login page

--- src/components/Pages/Login.jsx
+++ src/components/Pages/Login.jsx
@@ -1,7 +1,44 @@
 export default function Login(){
     return (
-        <div>
-            Login
+        <div className="min-h-screen  p-7">
+            {/* App logo */}
+            <p className="mb-10 text-sm">Torna alla home</p>
+            <div className="mx-auto max-w-md flex flex-col items-center">
+                <h1 className="text-black text-5xl font-extrabold mb-12">
+                    <span className="font-extralight">my</span>Finance     
+                </h1>
+                <div className="w-full">
+                    <div className="text-center mb-12">
+                        <h2 className="text-3xl font-medium mb-2">Accedi al tuo account</h2>
+                        <p className="text-zinc-700">Inizia a gestire le tue finanze</p>
+                    </div>
+                    
+                    {/* Login Form */}
+                    <form 
+                        className="w-full flex flex-col gap-7 p-5 border border-zinc-200 shadow-md rounded-md"
+                    >
+                        <p className="flex flex-col">
+                            <label htmlFor="email">Email</label>
+                            <input 
+                                type="email" 
+                                name="email" 
+                                id="email" 
+                            />
+                        </p>
+
+                        <p className="flex flex-col">
+                            <label htmlFor="password">Password</label>
+                            <input 
+                                type="password" 
+                                name="password" 
+                                id="password" 
+                            />
+                        </p>
+
+                    </form>
+                </div>
+            </div>
+
         </div>
     );    
 }
